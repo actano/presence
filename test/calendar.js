275ceb0a4ac6332492b2c8f366cb8d8ffc398061
Marcus Mennemeier <m.mennemeier@global-communication.de>
2016-04-30 21:28:11 +0200

    decaffeinate

--- test/calendar.js
+++ test/calendar.js
@@ -1,127 +1,154 @@
-describe 'ical', ->
-    EVENT_COUNT = 1
-    TEST_EVENT = 'Test'
-    TEST_DESCRIPTION = 'Test Description'
-    TEST_USER = 'Test User'
+describe('ical', function() {
+    let EVENT_COUNT = 1;
+    let TEST_EVENT = 'Test';
+    let TEST_DESCRIPTION = 'Test Description';
+    let TEST_USER = 'Test User';
 
-    {fs, path, Calendar, expect} = {}
+    let {fs, path, Calendar, expect} = {};
 
 
-    before 'require', ->
-        fs = require 'fs'
-        path = require 'path'
-        {expect} = require 'chai'
+    before('require', function() {
+        fs = require('fs');
+        path = require('path');
+        ({expect} = require('chai'));
 
-        Calendar = require '../lib/calendar'
+        return Calendar = require('../lib/calendar');
+    });
 
-    read = ->
-        content = fs.readFileSync path.join(__dirname, 'test.ics'), 'utf-8'
-        new Calendar content
+    let read = function() {
+        let content = fs.readFileSync(path.join(__dirname, 'test.ics'), 'utf-8');
+        return new Calendar(content);
+    };
 
-    it 'should parse ics data', ->
-        read()
+    it('should parse ics data', () => read());
 
-    describe 'process', ->
-        calendar = null
+    return describe('process', function() {
+        let calendar = null;
 
-        findEvent = (name) ->
-            eventIterator = calendar.events()
-            until (item = eventIterator.next()).done
-                event = item.value
-                return event if name is event.name()
+        let findEvent = function(name) {
+            let item;
+            let eventIterator = calendar.events();
+            while (!(item = eventIterator.next()).done) {
+                let event = item.value;
+                if (name === event.name()) { return event; }
+            }
+        };
 
-        before 'read', -> calendar = read()
+        before('read', () => calendar = read());
 
-        it "should iterate #{EVENT_COUNT} events", ->
-            eventIterator = calendar.events()
-            count = 0
-            until eventIterator.next().done
-                count++
-            expect count
-                .to.equal EVENT_COUNT
+        it(`should iterate ${EVENT_COUNT} events`, function() {
+            let eventIterator = calendar.events();
+            let count = 0;
+            while (!eventIterator.next().done) {
+                count++;
+            }
+            return expect(count)
+                .to.equal(EVENT_COUNT);
+        });
 
-        it "should find '#{TEST_EVENT}'", ->
-            event = findEvent TEST_EVENT
-            expect event, TEST_EVENT
-            .to.exist
+        it(`should find '${TEST_EVENT}'`, function() {
+            let event = findEvent(TEST_EVENT);
+            return expect(event, TEST_EVENT)
+            .to.exist;
+        });
 
-        describe "Event '#{TEST_EVENT}'", ->
-            moment = require 'moment'
-            str = (moment) -> if moment? then moment.format 'YYYY-MM-DD HH:mm' else 'null'
+        return describe(`Event '${TEST_EVENT}'`, function() {
+            let confluenceType;
+            let nextWeek;
+            let christmas;
+            let newYear;
+            let endMoment;
+            let moment = require('moment');
+            let str = function(moment) { if (moment != null) { return moment.format('YYYY-MM-DD HH:mm'); } else { return 'null'; } };
 
-            TEST_MOMENT = moment '2015-10-29'
-                .startOf 'day'
+            let TEST_MOMENT = moment('2015-10-29')
+                .startOf('day');
 
-            event = null
-            before 'find', ->
-                event = findEvent TEST_EVENT
+            let event = null;
+            before('find', () => event = findEvent(TEST_EVENT));
 
-            it "should have Description of '#{TEST_DESCRIPTION}'", ->
-                expect event.description()
-                    .to.equal TEST_DESCRIPTION
+            it(`should have Description of '${TEST_DESCRIPTION}'`, () =>
+                expect(event.description())
+                    .to.equal(TEST_DESCRIPTION)
+            );
 
-            it "should be in confluence subcalendar '#{confluenceType = 'leaves'}'", ->
-                expect event.confluenceCalendarType()
-                    .to.equal confluenceType
+            it(`should be in confluence subcalendar '${confluenceType = 'leaves'}'`, () =>
+                expect(event.confluenceCalendarType())
+                    .to.equal(confluenceType)
+            );
 
-            it "should be recurring", ->
-                expect event.icalEvent.isRecurring()
+            it("should be recurring", () =>
+                expect(event.icalEvent.isRecurring())
                     .to.be.true
+            );
 
-            it "should start on #{str TEST_MOMENT}", ->
-                startDate = event.startDate()
-                expect startDate, 'startDate'
-                .to.exist
+            it(`should start on ${str(TEST_MOMENT)}`, function() {
+                let startDate = event.startDate();
+                expect(startDate, 'startDate')
+                .to.exist;
 
-                expect startDate.isSame TEST_MOMENT, 'day'
-                .to.be.true
+                return expect(startDate.isSame(TEST_MOMENT, 'day'))
+                .to.be.true;
+            });
 
-            it "first occurance should be on #{str TEST_MOMENT}", ->
-                instanceIterator = event.instances()
-                {date} = instanceIterator.next().value
-                expect date.isSame TEST_MOMENT, 'day'
-                    .to.be.true
+            it(`first occurance should be on ${str(TEST_MOMENT)}`, function() {
+                let instanceIterator = event.instances();
+                let {date} = instanceIterator.next().value;
+                return expect(date.isSame(TEST_MOMENT, 'day'))
+                    .to.be.true;
+            });
 
-            it "starting a week later, first occurance should be on #{str nextWeek = moment(TEST_MOMENT).add 1, 'weeks'}", ->
-                instanceIterator = event.instances nextWeek
-                {date} = instanceIterator.next().value
-                expect date.isSame nextWeek, 'day'
-                    .to.be.true
+            it(`starting a week later, first occurance should be on ${str(nextWeek = moment(TEST_MOMENT).add(1, 'weeks'))}`, function() {
+                let instanceIterator = event.instances(nextWeek);
+                let {date} = instanceIterator.next().value;
+                return expect(date.isSame(nextWeek, 'day'))
+                    .to.be.true;
+            });
 
-            it "first occurance should stay on #{str TEST_MOMENT}, when iterating from a day before", ->
-                instanceIterator = event.instances moment(TEST_MOMENT).subtract 1, 'days'
-                {date} = instanceIterator.next().value
-                expect date.isSame(TEST_MOMENT, 'day'), str date
-                    .to.be.true
+            it(`first occurance should stay on ${str(TEST_MOMENT)}, when iterating from a day before`, function() {
+                let instanceIterator = event.instances(moment(TEST_MOMENT).subtract(1, 'days'));
+                let {date} = instanceIterator.next().value;
+                return expect(date.isSame(TEST_MOMENT, 'day'), str(date))
+                    .to.be.true;
+            });
 
-            it "should be excluded between #{str christmas = moment '2015-12-24'} and #{str newYear = moment '2016-01-01'}", ->
-                beforeChristmas = christmas.subtract 1, 'days'
-                instanceIterator = event.instances beforeChristmas
-                {date} = instanceIterator.next().value
-                expect date.isBefore(newYear, 'day'), str date
-                    .to.be.false
+            it(`should be excluded between ${str(christmas = moment('2015-12-24'))} and ${str(newYear = moment('2016-01-01'))}`, function() {
+                let beforeChristmas = christmas.subtract(1, 'days');
+                let instanceIterator = event.instances(beforeChristmas);
+                let {date} = instanceIterator.next().value;
+                return expect(date.isBefore(newYear, 'day'), str(date))
+                    .to.be.false;
+            });
 
-            it "should end before #{str endMoment = moment '2017-01-01'}", ->
-                instanceIterator = event.instances endMoment
-                instance = instanceIterator.next().value
-                expect instance, if instance? then str instance.date else null
-                    .to.not.exist
+            it(`should end before ${str(endMoment = moment('2017-01-01'))}`, function() {
+                let instanceIterator = event.instances(endMoment);
+                let instance = instanceIterator.next().value;
+                return expect(instance, (instance != null) ? str(instance.date) : null)
+                    .to.not.exist;
+            });
 
-            testAttendee = (cn) ->
-                it "should have #{cn} as attendee", ->
-                    attendeeIterator = event.attendees()
-                    attendee = null
-                    until (item = attendeeIterator.next()).done
-                        attendee = item.value
-                        break if attendee.cn() is cn
-                    expect attendee
-                        .to.exist
-                    expect attendee.cn()
-                        .to.equal cn
+            let testAttendee = cn =>
+                it(`should have ${cn} as attendee`, function() {
+                    let item;
+                    let attendeeIterator = event.attendees();
+                    let attendee = null;
+                    while (!(item = attendeeIterator.next()).done) {
+                        attendee = item.value;
+                        if (attendee.cn() === cn) { break; }
+                    }
+                    expect(attendee)
+                        .to.exist;
+                    return expect(attendee.cn())
+                        .to.equal(cn);
+                })
+            ;
 
-            testAttendee "#{TEST_USER}"
-            testAttendee "#{TEST_USER}1"
-            testAttendee "#{TEST_USER}2"
+            testAttendee(`${TEST_USER}`);
+            testAttendee(`${TEST_USER}1`);
+            return testAttendee(`${TEST_USER}2`);
+        });
+    });
+});
 
 
 
