c289c7b173f0ce150715a7988e89d3dbaa25ae0e
Marcus Mennemeier <m.mennemeier@global-communication.de>
2016-04-30 21:30:00 +0200

    decaffeinate

--- test/config.js
+++ test/config.js
@@ -1,16 +1,19 @@
-{expect} = require 'chai'
+import { expect } from 'chai';
 
-describe 'configuration', ->
-    {config, moment} = {}
+describe('configuration', function() {
+    let {config, moment} = {};
 
-    before 'require', ->
-        moment = require 'moment'
-        config = require '../lib/config'
+    before('require', function() {
+        moment = require('moment');
+        return config = require('../lib/config');
+    });
 
-    it 'should have correct start date', ->
-        _config = config moment '2015-10-01'
-        team = _config.teams[0]
-        expect team
-            .to.exist
-        expect team.sprint.startDate
-            .to.equal '2015-09-30'
+    return it('should have correct start date', function() {
+        let _config = config(moment('2015-10-01'));
+        let team = _config.teams[0];
+        expect(team)
+            .to.exist;
+        return expect(team.sprint.startDate)
+            .to.equal('2015-09-30');
+    });
+});
