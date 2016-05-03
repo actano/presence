962cd700e3e88e6ad8ea87234f4846c75966bbc6
Marcus Mennemeier <marcus@mennemeier.de>
2016-05-03 17:14:15 +0200

    move modelling to separate file

---
+++ views/model.js
@@ -0,0 +1,92 @@
+import seedrandom from 'seedrandom'
+import absenceClass from './absence-class'
+import moment from 'moment'
+
+export default function teamModel(team, currentDate) {
+    let result = {
+        name: team.name,
+        startOfBusiness: team.startOfBusiness,
+        endOfBusiness: team.endOfBusiness,
+        range: dateRange(team, currentDate),
+        members: []
+    };
+    for (let member of team.members) {
+        result.members.push(memberModel(member, result.range.start, result.range.end));
+    }
+    if (team.sprint.scrum) {
+        let avail = [];
+        let key = dateKey(currentDate);
+        for (let member of result.members) {
+            let absences = member.absences[key];
+            if (!absences) {
+                avail.push(member);
+            }
+        }
+        if (avail.length) {
+            let rng = seedrandom(key);
+            avail[Math.floor(rng() * avail.length)].selected = true;
+        }
+
+        result.sprint = {
+            number: team.sprint.count + 1,
+            start: team.sprint.start,
+            end: team.sprint.end,
+            summary: team.sprintSummary()
+        }
+    }
+    return result;
+}
+
+function dateRange(team, currentDate) {
+    let sprint = team.sprint.scrum ? {start: team.sprint.start, end: team.sprint.end} : null;
+
+    // start at start of current week
+    let start = currentDate.clone().weekday(0);
+    // end at end of next week
+    let end = start.clone().add('day', 13);
+    if (sprint) {
+        // show at least the current sprint
+        start = moment.min(start, sprint.start);
+        end = moment.max(end, sprint.end);
+    }
+
+    return {
+        currentDate,
+        start,
+        end,
+        sprint
+    };
+}
+
+function memberModel(member, start = null, end = start) {
+    let result = {
+        name: member.name,
+        absences: {}
+    };
+    if (start) {
+        for (let absence of member.absences(start, end)) {
+            let key = dateKey(absence.date);
+            let absences = result.absences[key];
+            if (!absences) {
+                result.absences[key] = absences = [];
+            }
+            absences.push(model(absence));
+        }
+    }
+    return result;
+}
+
+function model(absence) {
+    let zero = absence.date.clone().startOf('day');
+    return {
+        id: absence.event.icalEvent.uid,
+        className: absenceClass(absence),
+        start: absence.date.diff(zero, 'minutes'),
+        end: absence.day.endDate().diff(zero, 'minutes')
+    }
+}
+
+function dateKey(date) {
+    return date.format('YYYY-MM-DD');
+}
+
--- views/teams.jsx
+++ views/teams.jsx
@@ -1,36 +1,13 @@
 import React from 'react'
-import moment from 'moment'
 import config from '../lib/config'
 import Status from './status'
 import TeamHeadline from './team-headline'
 import Absences from './absences'
-import absenceClass from './absence-class'
 import Calendar from './calendar'
-import seedrandom from 'seedrandom'
+import teamModel from './model'
 
 function dateKey(date) {
     return date.format('YYYY-MM-DD');
-}
-
-function dateRange(team, currentDate) {
-    let sprint = team.sprint.scrum ? {start: team.sprint.start, end: team.sprint.end} : null;
-
-    // start at start of current week
-    let start = currentDate.clone().weekday(0);
-    // end at end of next week
-    let end = start.clone().add('day', 13);
-    if (sprint) {
-        // show at least the current sprint
-        start = moment.min(start, sprint.start);
-        end = moment.max(end, sprint.end);
-    }
-    
-    return {
-        currentDate,
-        start,
-        end,
-        sprint
-    };
 }
 
 class Head extends React.Component {
@@ -40,34 +17,6 @@ class Head extends React.Component {
             <div><img src={`${config.gravatarUrlFromName(name)}?s=${40}`}/><span>{name}</span></div>
         );
     }
-}
-
-function model(absence) {
-    let zero = absence.date.clone().startOf('day');
-    return {
-        id: absence.event.icalEvent.uid,
-        className: absenceClass(absence),
-        start: absence.date.diff(zero, 'minutes'),
-        end: absence.day.endDate().diff(zero, 'minutes')
-    }
-}
-
-function memberModel(member, start = null, end = start) {
-    let result = {
-        name: member.name,
-        absences: {}
-    };
-    if (start) {
-        for (let absence of member.absences(start, end)) {
-            let key = dateKey(absence.date);
-            let absences = result.absences[key];
-            if (!absences) {
-                result.absences[key] = absences = [];
-            }
-            absences.push(model(absence));
-        }
-    }
-    return result;
 }
 
 class Cell extends React.Component {
@@ -87,41 +36,6 @@ class Foot extends React.Component {
         let width = `${avail/total * 100}%`;
         return (<div className="percentage" style={{width}}>{avail}/{total}d available</div>)
     }
-}
-
-function teamModel(team, currentDate) {
-    let result = {
-        name: team.name,
-        startOfBusiness: team.startOfBusiness,
-        endOfBusiness: team.endOfBusiness,
-        range: dateRange(team, currentDate),
-        members: []
-    };
-    for (let member of team.members) {
-        result.members.push(memberModel(member, result.range.start, result.range.end));
-    }
-    if (team.sprint.scrum) {
-        let avail = [];
-        let key = dateKey(currentDate);
-        for (let member of result.members) {
-            let absences = member.absences[key];
-            if (!absences) {
-                avail.push(member);
-            }
-        }
-        if (avail.length) {
-            let rng = seedrandom(key);
-            avail[Math.floor(rng() * avail.length)].selected = true;
-        }
-
-        result.sprint = {
-            number: team.sprint.count + 1,
-            start: team.sprint.start,
-            end: team.sprint.end,
-            summary: team.sprintSummary()
-        }
-    }
-    return result;
 }
 
 class Team extends React.Component {
