fc135bf617a8613a8946d62d3f6003c372a3f1e6
Marcus Mennemeier <marcus@mennemeier.de>
2016-05-11 13:46:00 +0200

    use content-negotiation to allow application/json

--- index.js
+++ index.js
@@ -14,27 +14,46 @@ app.use(express.static('build'));
 
 // respond with rendered html
 app.get('/', function(req, res, next) {
-    let date;
-    if (req.query.date) {
-        date = moment(req.query.date);
-        if (!date.isValid()) date = null;
+    function getDate(dateParam) {
+        let date;
+        if (dateParam) {
+            date = moment(dateParam);
+            if (!date.isValid()) date = null;
+        }
+        if (!date) date = moment();
+        date = date.locale('de_DE').startOf('day');
+        return date;
     }
-    if (!date) date = moment();
-    date = date.locale('de_DE').startOf('day');
 
-    let framed = !!req.query.framed;
+    function html() {
+        let date = getDate(req.query.date);
+        let framed = !!req.query.framed;
 
-    presence(date).then((teams) => {
-        let props = {
-            teams,
-            framed,
-            date: date.format('YYYY-MM-DD')
-        };
+        presence(date).then((teams) => {
+            let props = {
+                teams,
+                framed,
+                date: date.format('YYYY-MM-DD')
+            };
 
-        let pageElement = React.createElement(Page, props);
-        let html = ReactDOMServer.renderToString(pageElement);
-        res.send('<!DOCTYPE html>' + html);
-    }).catch(next);
+            let pageElement = React.createElement(Page, props);
+            let html = ReactDOMServer.renderToString(pageElement);
+            res.send('<!DOCTYPE html>' + html);
+        }).catch(next);
+    }
+
+    function json() {
+        let date = getDate(req.query.date);
+        presence(date).then((teams) => {
+            res.send(teams);
+        }).catch(next);
+    }
+
+    res.format({
+        'text/html': html,
+        'application/json': json,
+        'default': html
+    });
 });
 
 let port = process.env.PORT || 3000;
