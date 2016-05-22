d0e4b48bce72df23d5b746d1b0117f73714a0659
Marcus Mennemeier <marcus@mennemeier.de>
2016-05-22 16:15:15 +0200

    push moment down from lib/updateAbsence.js

--- lib/views/status.jsx
+++ lib/views/status.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import moment from 'moment'
 
 export default class Status extends React.Component {
     render() {
         return (
-            <h2 className="error">Calendar failed: {this.props.status}, loading data from cache ({this.props.lastModified.format('L LT')})</h2>
+            <h2 className="error">Calendar failed: {this.props.status}, loading data from cache ({moment(this.props.lastModified).format('L LT')})</h2>
         );
     }
 }
