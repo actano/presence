19031d102487c7ba5cc31bea9de4a55aefa022ee
Marcus Mennemeier <marcus@mennemeier.de>
2016-06-07 21:09:42 +0200

    eslint autofix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from 'express'
 import ReactDOMServer from 'react-dom/server'
 import socketio from 'socket.io'
 import { LocalDate } from 'js-joda'
@@ -7,79 +7,79 @@ import Page from './lib/views'
 import React from 'react'
 import config from './lib/config'
 
-const app = express();
+const app = express()
 
-app.locals.compileDebug = false;
+app.locals.compileDebug = false
 
-app.use(express.static('build'));
+app.use(express.static('build'))
 
 function getDate(dateParam) {
-    let date;
-    if (dateParam) {
-        try {
-            date = LocalDate.parse(dateParam);
-        } catch (e) {
-            date = null;
-        }
+  let date
+  if (dateParam) {
+    try {
+      date = LocalDate.parse(dateParam)
+    } catch (e) {
+      date = null
     }
-    if (!date) date = LocalDate.now();
-    return date;
+  }
+  if (!date) date = LocalDate.now()
+  return date
 }
 
-app.get('/', function(req, res, next) {
+app.get('/', function (req, res, next) {
 
-    function html() {
-        let framed = !!req.query.framed;
+  function html() {
+    let framed = !!req.query.framed
 
-        let props = {
-            framed
-        };
-
-        let pageElement = React.createElement(Page, props);
-        let html = ReactDOMServer.renderToStaticMarkup(pageElement);
-        res.send('<!DOCTYPE html>' + html);
+    let props = {
+      framed,
     }
 
-    function json() {
-        let date = getDate(req.query.date);
-        let _config = config(date);
-        presence(date).then((teams) => {
-            res.send({
-                teams: teams,
-                gravatarPrefix: _config.gravatarPrefix,
-                emailSuffix: _config.emailSuffix
-            });
-        }).catch(next);
-    }
+    let pageElement = React.createElement(Page, props)
+    let html = ReactDOMServer.renderToStaticMarkup(pageElement)
+    res.send('<!DOCTYPE html>' + html)
+  }
 
-    res.format({
-        'text/html': html,
-        'application/json': json,
-        'default': html
-    });
-});
+  function json() {
+    let date = getDate(req.query.date)
+    let _config = config(date)
+    presence(date).then((teams) => {
+      res.send({
+        teams: teams,
+        gravatarPrefix: _config.gravatarPrefix,
+        emailSuffix: _config.emailSuffix,
+      })
+    }).catch(next)
+  }
 
-let port = process.env.PORT || 3000;
+  res.format({
+    'text/html': html,
+    'application/json': json,
+    'default': html,
+  })
+})
 
-const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
-const io = socketio(server, {serveClient: false, path: '/rt'});
+let port = process.env.PORT || 3000
+
+const server = app.listen(port, () => console.log(`Listening on port ${port}...`))
+const io = socketio(server, { serveClient: false, path: '/rt' })
 io.on('connection', (client) => {
-    client.on('date', (date) => {
-        date = getDate(date);
-        let _config = config(date);
-        presence(date).then((teams) => {
-            client.emit('teams', {
-                date: date.toString(),
-                teams: teams,
-                gravatarPrefix: _config.gravatarPrefix,
-                emailSuffix: _config.emailSuffix
-            });
-        });
-    });
-});
+  client.on('date', (date) => {
+    date = getDate(date)
+    let _config = config(date)
+    presence(date).then((teams) => {
+      client.emit('teams', {
+        date: date.toString(),
+        teams: teams,
+        gravatarPrefix: _config.gravatarPrefix,
+        emailSuffix: _config.emailSuffix,
+      })
+    })
+  })
+})
 
 // dumb pushing out of update events every 5 minutes
-setInterval(()=> {
-    io.emit('update')
-}, 5 * 60 * 1000);
+setInterval(() => {
+  io.emit('update')
+}, 5 * 60 * 1000)
 
