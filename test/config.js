60d013202e5afed1fac6d07f0cae7a5f1ccff92d
Marcus Mennemeier <marcus.mennemeier@actano.de>
2019-01-10 15:37:24 +0100

    Make config async

--- test/config.js
+++ test/config.js
@@ -6,8 +6,8 @@ import config from '../lib/server/config'
 /* eslint-disable no-unused-expressions */
 
 describe('configuration', () => {
-  it('should have correct start date', () => {
-    const _config = config(LocalDate.parse('2015-10-01'))
+  it('should have correct start date', async () => {
+    const _config = await config(LocalDate.parse('2015-10-01'))
     const team = _config.teams[0]
     expect(team).to.exist
     expect(team.sprint.startDate).to.equal('2015-09-30')
