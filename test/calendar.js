f20bd522687b76d50f20d592f6a3d97b9d08d2bf
Marcus Mennemeier <marcus.mennemeier@actano.de>
2019-01-15 14:02:22 +0100

    Turn startDate() and endDate() into properties

--- test/calendar.js
+++ test/calendar.js
@@ -3,7 +3,7 @@ import { expect } from 'chai'
 import { Instant, LocalDate } from 'js-joda'
 import icsFromURL from '../lib/server/ics-from-url'
 import { toLocalDate } from '../lib/server/util'
-import events from '../lib/server/calendar'
+import events, { instances, instancesAfter } from '../lib/server/calendar'
 
 /* eslint-env mocha */
 /* eslint-disable no-unused-expressions */
@@ -70,20 +70,20 @@ describe('ical', () => {
         expect(event.icalEvent.isRecurring()).to.be.true)
 
       it(`should start on ${TEST_DAY}`, () => {
-        const startDate = event.startDate()
+        const startDate = event.startDate
         expect(startDate, 'startDate').to.exist
         expect(TEST_DAY.equals(toLocalDate(startDate))).to.be.true
       })
 
       let testMoment
       it(`should start at ${testMoment = Instant.parse('2015-10-29T12:30:00Z')}`, () => {
-        const startDate = event.startDate()
+        const startDate = event.startDate
         expect(startDate.equals(testMoment)).to.be.true
       })
 
       it(`first occurance should be on ${TEST_DAY}`, () => {
-        for (const instance of event.instances()) {
-          const date = toLocalDate(instance.startDate())
+        for (const instance of instances(event)) {
+          const date = toLocalDate(instance.startDate)
           expect(date.equals(TEST_DAY)).to.be.true
           return
         }
@@ -92,8 +92,8 @@ describe('ical', () => {
 
       const nextWeek = TEST_DAY.plusWeeks(1)
       it(`starting a week later, first occurance should be on ${nextWeek}`, () => {
-        for (const instance of event.instances(nextWeek)) {
-          const date = toLocalDate(instance.startDate())
+        for (const instance of instancesAfter(event, nextWeek)) {
+          const date = toLocalDate(instance.startDate)
           expect(date.equals(nextWeek)).to.be.true
           return
         }
@@ -101,8 +101,8 @@ describe('ical', () => {
       })
 
       it(`first occurance should stay on ${TEST_DAY}, when iterating from a day before`, () => {
-        for (const instance of event.instances(TEST_DAY.minusDays(1))) {
-          const date = toLocalDate(instance.startDate())
+        for (const instance of instancesAfter(event, TEST_DAY.minusDays(1))) {
+          const date = toLocalDate(instance.startDate)
           expect(date.equals(TEST_DAY), date.toString()).to.be.true
           return
         }
@@ -113,8 +113,8 @@ describe('ical', () => {
       const newYear = LocalDate.parse('2016-01-01')
       it(`should be excluded between ${christmas} and ${newYear}`, () => {
         const beforeChristmas = christmas.minusDays(1)
-        for (const instance of event.instances(beforeChristmas)) {
-          const date = toLocalDate(instance.startDate())
+        for (const instance of instancesAfter(event, beforeChristmas)) {
+          const date = toLocalDate(instance.startDate)
           expect(newYear.compareTo(date), date.toString()).to.be.below(0)
           return
         }
@@ -123,8 +123,8 @@ describe('ical', () => {
 
       const endMoment = LocalDate.parse('2017-01-01')
       it(`should end before ${endMoment}`, () => {
-        for (const instance of event.instances(endMoment)) {
-          const date = instance.startDate()
+        for (const instance of instancesAfter(event, endMoment)) {
+          const date = instance.startDate
           expect(instance, date.toString()).to.not.exist
         }
       })
